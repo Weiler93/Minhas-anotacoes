ecfa4ec2584aa8485d8970f9ad9336243669a406
weiler nogueira <weilernogueira@gmail.com>
2024-05-07 12:12:32 -0300

    pasta adicionda como exercicios com arquivo exercicio 01.js

---
+++ exercicios/exercio01.js
@@ -0,0 +1,13 @@
+let a;
+console.log("valor inicial de a:", a);
+a = prompt("Digite um número: ");
+console.log("resultado de a:", a)
+alert("resultado de a:", a)
+
+let nome;
+// nome = 'weiler';
+nome=prompt("qual o primeiro nome? ")
+let sobrenome;
+sobrenome =prompt("qual o sobrenome? ") ;
+console.log("seja bem vindo"+nome+sobrenome)
+alert("seja bem vindo" +nome+sobrenome)

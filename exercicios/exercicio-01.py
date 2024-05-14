811b98ac8bf7a26bfadca8293d32da2a5d04fc82
weiler nogueira <weilernogueira@gmail.com>
2024-05-14 11:36:53 -0300

    exercicio-01.py nome e sobrenome concluido

---
+++ exercicios/exercicio-01.py
@@ -0,0 +1,18 @@
+nome = input('Digite o nome: ')
+sobrenome = input('Digite o sobrenome: ')
+
+soma = nome + sobrenome
+
+
+print(type(soma))
+
+print(str(soma))
+
+
+# na linha de comando py foi imprimido da senguinte forma:
+$ py exercicio-01.py 
+Digite o nome: weiler
+Digite o sobrenome: jah
+<class 'str'>
+weilerjah
+

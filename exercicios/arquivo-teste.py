c8652a60795cc7ffabad12f273471ffb2a7b2784
weiler nogueira <weilernogueira@gmail.com>
2024-05-14 10:40:16 -0300

    arquivo de exercicios python

---
+++ exercicios/arquivo-teste.py
@@ -0,0 +1,12 @@
+
+
+a = int(input('Digite o primeiro numero:'))
+b = int(input('Digite o segundo numero:'))
+
+soma = a + b 
+
+print(type(soma))
+
+print('o resultado da soma é ' +  str(soma))
+
+

ba32ed5009d9ac3b3f8d78e9e4e9a71c12900add
weiler nogueira <weilernogueira@gmail.com>
2024-05-29 10:06:02 -0300

    novos arquivos criados cond.py e cond.js

---
+++ exercicios/condp.js
@@ -0,0 +1,19 @@
+// console.log("Try programiz.pro");
+// let lista =[2,5,10,-8,20,99]
+// let N = lista.length;
+// let soma = 0
+// for(let i = 0;i < N ;i =i+1){
+//     let x = lista[i];
+//     soma = soma + x;
+// console.log(soma);
+// }
+
+console.log("Try programiz.pro");
+let lista =[2,5,10,-8,20,99]
+let N = lista.length;
+let soma = 0
+for(let i = 0;i < N ;i =i+1){
+    let x = lista[i];
+    soma = soma + x;
+}
+console.log(soma);
---
+++ exercicios/condp.py
@@ -0,0 +1,17 @@
+# print("Try programiz.pro")
+# lista =[2,5,10,-8,20,99]
+# N = len(lista)
+# soma = 0
+# for i in range(N):
+#     x = lista[i]
+#     soma = soma+x
+# print(soma)
+
+print("Try programiz.pro")
+lista =[2,5,10,-8,20,99]
+N = len(lista)
+soma = 0
+for i in range(N):
+    x = lista[i]
+    soma = soma+x
+print(soma)

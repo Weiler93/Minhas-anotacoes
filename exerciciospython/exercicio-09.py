90bf2c38f25ea014237b2c1e15401abb2fbd7aa4
weiler nogueira <weilernogueira@gmail.com>
2024-06-07 12:03:32 -0300

    exemplo exercico 09 lista

---
+++ exerciciospython/exercicio-09.py
@@ -0,0 +1,6 @@
+##9. Escreva um programa que solicita um número ao usuário e imprime o seu dobro.
+
+a = input("digite um numero: ")
+n1 = int(a)
+dobro = n1*2
+print("o dobro do numero é: " + str(dobro))
